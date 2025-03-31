ff8ff7082896200d8c787d19ee531f6bd5766540
mahindar5 <mahindar.275@gmail.com>
2025-03-30 19:13:43 -0700

    feat: add common-lib dependency and integrate fetchProxy in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { fetchProxy } from '@mahindar5/common-lib';
 import { AppComponent, appConfig } from '@mahindar5/ng-common-mat-lib';
 
+fetchProxy();
 bootstrapApplication(AppComponent, appConfig)
 	.catch((err) => console.error(err));
