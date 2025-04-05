e12546c4b697712e70dd110ffb0c3678d0167ba5
mahindar5 <mahindar.275@gmail.com>
2025-04-04 19:34:23 -0700

    feat: update build-deploy-pages workflow and clean up main.ts imports

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BUILD_INFO as commonLibBUILD_INFO, fetchProxy } from '@mahindar5/common-lib';
-import { appConfig, configureApplicationWithConfig, BUILD_INFO as matBUILD_INFO } from '@mahindar5/ng-common-mat-lib';
+import { configureApplicationWithConfig, BUILD_INFO as matBUILD_INFO } from '@mahindar5/ng-common-mat-lib';
 import { AppComponent, } from '@mahindar5/ng-common-mat-lib/components/app';
 import { BUILD_INFO as myAppBUILD_INFO } from './build-info';
 
 
 fetchProxy();
-bootstrapApplication(AppComponent, configureApplicationWithConfig({ BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO }, appConfig }))
+bootstrapApplication(AppComponent, configureApplicationWithConfig({ BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO } }))
 	.catch((err) => console.error(err));
